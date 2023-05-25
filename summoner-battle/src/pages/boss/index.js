1eb194c53764092f4a2e33f013f5803608c42cf4
Heedo Kim <devheedoo@gmail.com>
2023-05-25 23:07:20 +0900

    feat(boss): 웹소켓 서버 연결

--- summoner-battle/src/pages/boss/index.js
+++ summoner-battle/src/pages/boss/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 import { Battle } from '../../features/battle/Battle';
+import { socket } from '../../socket';
+
 function Main() {
-    return (
+  const [isSocketServerConnected, setIsSocketServerConnected] = useState(
+    socket.connected
+  );
+
+  useEffect(() => {
+    socket.on('connect', () => setIsSocketServerConnected(true));
+    socket.on('disconnect', () => setIsSocketServerConnected(false));
+    socket.on('hello', () => console.log('Hello! from Server'));
+  }, []);
+
+  return (
     <div className="App">
       <h1>보스모드</h1>
+
+      <p>웹소켓 서버 연결 상태: {'' + isSocketServerConnected}</p>
+      <button onClick={() => socket.connect()}>연결</button>
+      <button onClick={() => socket.disconnect()}>해제</button>
+
       <Battle id="1" />
       <Battle id="2" />
       <Link to="/">
-          <button>뒤로가기</button>
+        <button>뒤로가기</button>
       </Link>
     </div>
   );
